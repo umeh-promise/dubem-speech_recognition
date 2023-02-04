cd05bb853b617e8f54e3bf1abcd236bc53bd2214
Umeh Promise Chidubem <dubem@professor>
2023-02-04 18:05:50 +0100

    Python speech recognition script

---
+++ recognition.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+
+def speech_from_audio(recognizer):
+    """Transcribe speech from 'audio file'.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+
+    speech = sr.Recognizer()
+    
+    with recognizer as source:
+        speech.adjust_for_ambient_noise(source)
+        audio = speech.record(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = speech.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+recognizer = sr.AudioFile("./audio_files/promise.wav")
+
+speech_from_audio(recognizer)
